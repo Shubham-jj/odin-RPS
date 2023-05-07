b88cf20a7b3aa6aaaf41030746a9af5e532776fb
Shubham Joshi <shubhamjj1261@gmail.com>
2023-05-07 19:54:37 +0530

    Commit-Latest

--- RPS.js
+++ RPS.js
@@ -1,5 +1,7 @@
 // Getting Computer Choice.
 // let ComChoice;
+let cctr = 0;
+let pctr = 0;
 function getComputerChoice() {
   let rando = Math.floor(Math.random() * 3);
   if (rando == 0) {
@@ -16,37 +18,42 @@ function getComputerChoice() {
 
 function playRound(computer, player) {
   let result;
-
   if (computer == player) {
     result = `It's a tie because ${computer} and ${player} are same!`;
     return result;
   } else if (computer == "Rock" && player == "Paper") {
     result = `Player wins because ${player} beats ${computer}!`;
+    pctr++;
     return result;
   } else if (computer == "Rock" && player == "Scissors") {
     result = `Computer wins because ${computer} beats ${player}!`;
+    cctr++;
     return result;
   } else if (computer == "Paper" && player == "Rock") {
     result = `Computer wins because ${computer} beats ${player}!`;
-
+    cctr++;
     return result;
   } else if (computer == "Paper" && player == "Scissors") {
     result = `Player wins because ${player} beats ${computer}!`;
+    pctr++;
     return result;
   } else if (computer == "Scissors" && player == "Rock") {
     result = `Player wins because ${player} beats ${computer}!`;
+    pctr++;
     return result;
   } else if (computer == "Scissors" && player == "Paper") {
     result = `Computer wins beacuse ${computer} beats ${player}!`;
+    cctr++;
     return result;
   }
+  console.log(pctr, cctr);
 }
 
 function game() {
   for (let i = 1; i <= 5; i++) {
-    console.log(
-      playRound(getComputerChoice(), prompt("Choose Rock,Paper or Scissors: "))
-    );
+    y = prompt("Rock, Paper or Scissors??");
+    z = playRound(getComputerChoice(), y);
+    console.log(z);
   }
 }
 game();
